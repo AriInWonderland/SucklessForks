fdf2357d4d97ad55f173769028f9d81822c02069
AriInWonderland <alicewulfric@gmail.com>
2022-05-09 00:18:07 -0300

    Alt+V to vscode, UwU

--- dwm/config.h
+++ dwm/config.h
@@ -49,6 +49,7 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "",      tile },    /* first entry is default */
 	{ "",      NULL },    /* no layout function means floating behavior */
+    { "[M]",    monocle},
 };
 
 /* key definitions */
@@ -67,6 +68,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browser[]  = { "librewolf", "-p" };
+static const char *vscode[]   = { "vscode", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -106,6 +108,7 @@ static Key keys[] = {
 
     /* Custom shortcuts. */ 
 	{ MODKEY,                       XK_a,      spawn,          {.v = browser } },
+    { MODKEY,                       XK_v,      spawn,          {.v = vscode  } },
 };
 
 /* button definitions */
